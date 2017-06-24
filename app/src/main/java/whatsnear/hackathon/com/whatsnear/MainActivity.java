d4bfdca28400e8987e04577874bfd17f94ced76d
Shrenik Vikam <shvikam@microsoft.com>
2017-06-24 14:39:12 +0530

    updated build files added analytics.

--- app/src/main/java/whatsnear/hackathon/com/whatsnear/MainActivity.java
+++ app/src/main/java/whatsnear/hackathon/com/whatsnear/MainActivity.java
@@ -1,18 +1,17 @@
 package whatsnear.hackathon.com.whatsnear;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-
-import android.app.Activity;
-import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
     // Search EditText
     EditText inputSearch;
-
+    private FirebaseAnalytics mFirebaseAnalytics;
 
     // ArrayList for Listview
     ArrayList<HashMap<String, String>> productList;
@@ -35,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
         // Listview Data
         String products[] = {"Dell Inspiron", "HTC One X", "HTC Wildfire S", "HTC Sense", "HTC Sensation XE",
                 "iPhone 4S", "Samsung Galaxy Note 800",
@@ -58,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
-                                          int arg3) {
+                    int arg3) {
                 // TODO Auto-generated method stub
 
             }
